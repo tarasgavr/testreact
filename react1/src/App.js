4adb247ab82987b21d004887b063b41061698505
tarasgavr <tarasgavrilov1985@gmail.com>
2025-01-31 19:16:35 +0200

    go

---
+++ react1/src/App.js
@@ -0,0 +1,9 @@
+import Title from "./Title";
+function App() {
+
+  return (<div>
+    <Title />
+  </div>);
+}
+
+export default App;
---
+++ react1/src/Title.js
@@ -0,0 +1,4 @@
+function Title() {
+  return <h1>HELLO WORLD!</h1>;
+}
+export default Title;
