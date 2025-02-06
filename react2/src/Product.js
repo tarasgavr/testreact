964f50af38ea8812390f05c59e9f4c89e93bbac0
tarasgavr <tarasgavrilov1985@gmail.com>
2025-02-06 17:14:48 +0200

    go

--- react2/src/Product.js
+++ react2/src/Product.js
@@ -2,7 +2,7 @@ import "./Product.css"
 function Product({product}) {
   return <div className="product_table">
     <span className="product_name">Назва: {product.name}</span>
-    <span className="product_price">Ціна: {product.price}</span>
+    <span className="product_price">Ціна: ${product.price}</span>
     {product.inStock ? <span className="product_exists">Є в наявності</span> : <span className="product_exists-not">Немає в наявності</span>}
   </div>;
 }
