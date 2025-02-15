0caf0b12404470b5d0c3d35a6265471df7d7ae6f
tarasgavr <tarasgavrilov1985@gmail.com>
2025-02-15 20:06:20 +0200

    go

--- react2/src/App.js
+++ react2/src/App.js
@@ -1,14 +1,28 @@
 import Product from "./Product";
 import "./App.css"
 function App() {
-  const product = {
+  const data = [
+    {
       name: "iPhone13",
       price: "$999",
       inStock: true,
-    }
+    },
+    {
+      name: "iPhone14",
+      price: "$1499",
+      inStock: true,
+    },
+    {
+      name: "iPhone15",
+      price: "$1999",
+      inStock: true,
+    },
+  ]
+  const list = data.map(item => <Product product={item} />
+  )
   return (<div className='App'>
     <h1>СТОРІНКА ПРОДУКТУ</h1>
-    <Product product={product} />
+    {list}
   </div>);
 }
 
