2965b05a2e295a01f2347b554f80dbfd9f2389ac
tarasgavr <tarasgavrilov1985@gmail.com>
2025-03-02 20:02:08 +0200

    go

--- react3/src/App.js
+++ react3/src/App.js
@@ -1,14 +1,12 @@
-import { Button, Link } from "./components/index";
+import { Button, Link, buttonClick, linkClick } from "./components/index";
 import "./App.css"
-const click = (Txt) => {
-  alert(`You clicked ${ Txt }`)
-}
+
 function App() {
   return (<div className='App'>
     <h1>СТОРІНКА ПРИКЛАДУ</h1>
     <div className='table'>
-      <Button click={click("button")} buttonTxt="I'm button" />
-      <Link click={click("link")} linkTxt="I'm link" />
+      <Button click={buttonClick} buttonTxt="I'm button" />
+      <Link click={linkClick} linkTxt="I'm link" />
     </div>
   </div>);
 }
--- react3/src/components/index.js
+++ react3/src/components/index.js
@@ -1,7 +1,13 @@
-import "./index.css";
+import "./index.css"
 export function Button({ buttonTxt, click }) {
   return <button clasName="button" onClick={click}>{buttonTxt}</button >
 }
 export function Link({ linkTxt, click }) {
   return <a clasName="link" onClick={click}>{linkTxt}</a>;
+}
+export const buttonClick = () => {
+  alert(`You clicked button`)
+}
+export const linkClick = () => {
+  alert(`You clicked link`)
 }
