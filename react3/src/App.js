65066e1d1f3b84adc065c3646f69dd2ebda9b3ae
taras <tarasgavrilov@gmail.com>
2026-04-06 21:14:46 +0200

    пп

--- react3/src/App.js
+++ react3/src/App.js
@@ -2,13 +2,15 @@ import { Button, Link, buttonClick, linkClick } from "./components/index";
 import "./App.css"
 
 function App() {
-  return (<div className='App'>
+  return (
+  <div className='App'>
     <h1>СТОРІНКА ПРИКЛАДУ</h1>
     <div className='table'>
       <Button click={buttonClick} buttonTxt="I'm button" />
       <Link click={linkClick} linkTxt="I'm link" />
     </div>
-  </div>);
+  </div>
+  );
 }
 
 export default App;
